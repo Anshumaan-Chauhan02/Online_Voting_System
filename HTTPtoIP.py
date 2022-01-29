48e5ab754bea972fdc46bd700b1c826c3cec6961
Anshumaan-Chauhan02 <98472023+Anshumaan-Chauhan02@users.noreply.github.com>
2022-01-29 16:52:05 -0700

    Delete HTTPtoIP.py

--- HTTPtoIP.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def check_peer(resp):
-    orig_resp = resp.raw._original_response
-    if hasattr(orig_resp,'peer'):
-        return getattr(orig_resp,'peer')
-r1 = requests.get('http://www.google.com')
-check_peer(r1)
